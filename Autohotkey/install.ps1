c4cc33fd14ee66c4c9ebb7e8bf4c9fb79380dc41
karl-henrik <karl-henrik@mean.se>
2024-05-19 16:41:50 +0400

    Updated install script with correct file name

--- Autohotkey/install.ps1
+++ Autohotkey/install.ps1
@@ -4,6 +4,6 @@ echo $startupFolder;
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$startupFolder\autohotkey.lnk")
-$Shortcut.TargetPath = "$pwd\autohotkey.ahk"
+$Shortcut.TargetPath = "$pwd\karl-henrik.ahk"
 $Shortcut.WorkingDirectory = $pwd.Path
 $Shortcut.Save()
